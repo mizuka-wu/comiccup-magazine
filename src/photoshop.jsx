5ae957ee5baa37d9803809ad8b0b198e9bef81c0
吴贻韡 <mizuka.wu@outlook.com>
2020-06-22 16:03:01 +0800

    feat: 开放全部功能

--- src/photoshop.jsx
+++ src/photoshop.jsx
@@ -351,7 +351,7 @@ var meta = {
   ]
 }
 
-// alert(message)
+alert(message)
 
 // 获取容器， 中间/组1
 var root = app.activeDocument.layerSets.getByName(BODY_NAME).layerSets.getByName(CONTAINER_NAME)
@@ -361,64 +361,64 @@ var root = app.activeDocument.layerSets.getByName(BODY_NAME).layerSets.getByName
  * 也就是最后16个代表摊位的元素(4行，左右两边，摊位id + 名 两个textarea)
  * 危险操作，因为有循环时候增加数据的操作，操控不好会死循环
  */
-// for (var i = root.artLayers.length - ROW_NUMBER * 2 * 2; i < root.artLayers.length; i = i + COL_EACH_ROW) {
-//   var layer = root.artLayers[i]
-//   // 每行已经有一个默认的了，所以-1
-//   for (var j = 0; j < (COL_EACH_ROW - 1); j++) {
-//     var copy = layer.duplicate()
-//     // 从后往前，位置保证一致
-//     var offset = CELL_WIDTH_OFFSET * (COL_EACH_ROW - 1 - j)
-//     copy.translate(new UnitValue(offset + ' pixels'))
-//   }
-// }
+for (var i = root.artLayers.length - ROW_NUMBER * 2 * 2; i < root.artLayers.length; i = i + COL_EACH_ROW) {
+  var layer = root.artLayers[i]
+  // 每行已经有一个默认的了，所以-1
+  for (var j = 0; j < (COL_EACH_ROW - 1); j++) {
+    var copy = layer.duplicate()
+    // 从后往前，位置保证一致
+    var offset = CELL_WIDTH_OFFSET * (COL_EACH_ROW - 1 - j)
+    copy.translate(new UnitValue(offset + ' pixels'))
+  }
+}
 
 /**
  * 左边填本子名字和社团名字
  */
-// var left = meta.left
-// for (var leftIndex = 0; leftIndex < left.length; leftIndex++) {
-//   var artIndex = left.length - leftIndex - 1
-//   var book = left[leftIndex]
-//   // 改为本子名
-//   root.artLayers[artIndex].textItem.contents = book.name
-//   // 改对应的社团名 因为摊位名称不是成组复制的，需要通过取余数来判断偏移(x2因为空了一行)
-//   var groupIdIndex = root.artLayers.length - 1 - (leftIndex % COL_EACH_ROW) - Math.floor(leftIndex / COL_EACH_ROW) * COL_EACH_ROW * 2
-//   var stallNameIndex = groupIdIndex - COL_EACH_ROW
-//   root.artLayers[groupIdIndex].textItem.contents = book.groupId
-//   root.artLayers[stallNameIndex].textItem.contents = book.stallName
-//   // 同一个社团则隐藏名字
-//   if (lastGroupId === book.groupId) {
-//     root.artLayers[groupIdIndex].visible = false
-//     root.artLayers[stallNameIndex].visible = false
-//   }
-//   // 哨兵赋值
-//   lastGroupId = book.groupId
-// }
+var left = meta.left
+for (var leftIndex = 0; leftIndex < left.length; leftIndex++) {
+  var artIndex = left.length - leftIndex - 1
+  var leftBook = left[leftIndex]
+  // 改为本子名
+  root.artLayers[artIndex].textItem.contents = leftBook.name
+  // 改对应的社团名 因为摊位名称不是成组复制的，需要通过取余数来判断偏移(x2因为空了一行)
+  var leftBookGroupIdIndex = root.artLayers.length - 1 - (leftIndex % COL_EACH_ROW) - Math.floor(leftIndex / COL_EACH_ROW) * COL_EACH_ROW * 2
+  var leftBookStallNameIndex = leftBookGroupIdIndex - COL_EACH_ROW
+  root.artLayers[leftBookGroupIdIndex].textItem.contents = leftBook.groupId
+  root.artLayers[leftBookStallNameIndex].textItem.contents = leftBook.stallName
+  // 同一个社团则隐藏名字
+  if (lastGroupId === leftBook.groupId) {
+    root.artLayers[leftBookGroupIdIndex].visible = false
+    root.artLayers[leftBookStallNameIndex].visible = false
+  }
+  // 哨兵赋值
+  lastGroupId = leftBook.groupId
+}
 
 /**
  * 右边填本子名字和社团名字
  */
-// var right = meta.right
-// for (var rightIndex = 0; rightIndex < right.length; rightIndex++) {
-//   var book = right[rightIndex]
-//   // 本子名字填充
-//   var layerSetsIndex = ROW_NUMBER - Math.floor(rightIndex / COL_EACH_ROW) - 1 // 获取分组位子, 倒序
-//   var artLayerIndex = COL_EACH_ROW - (rightIndex % COL_EACH_ROW) - 1
-//   var layerSet = root.layerSets[layerSetsIndex]
-//   layerSet.artLayers[artLayerIndex].textItem.contents = book.name
-//   // 改对应的社团名 因为摊位名称不是成组复制的，需要通过取余数来判断偏移(x2因为空了一行), 顺便减去left所占用的格子（20 * 2）
-//   var groupIdIndex = (root.artLayers.length - ROW_NUMBER * COL_EACH_ROW * 2) - 1 - (rightIndex % COL_EACH_ROW) - Math.floor(rightIndex / COL_EACH_ROW) * COL_EACH_ROW * 2
-//   var stallNameIndex = groupIdIndex - COL_EACH_ROW
-//   root.artLayers[groupIdIndex].textItem.contents = book.groupId
-//   root.artLayers[stallNameIndex].textItem.contents = book.stallName
-//   // 同一个社团则隐藏名字
-//   if (lastGroupId === book.groupId) {
-//     root.artLayers[groupIdIndex].visible = false
-//     root.artLayers[stallNameIndex].visible = false
-//   }
-//   // 哨兵赋值
-//   lastGroupId = book.groupId
-// }
+var right = meta.right
+for (var rightIndex = 0; rightIndex < right.length; rightIndex++) {
+  var rightBook = right[rightIndex]
+  // 本子名字填充
+  var layerSetsIndex = ROW_NUMBER - Math.floor(rightIndex / COL_EACH_ROW) - 1 // 获取分组位子, 倒序
+  var artLayerIndex = COL_EACH_ROW - (rightIndex % COL_EACH_ROW) - 1
+  var layerSet = root.layerSets[layerSetsIndex]
+  layerSet.artLayers[artLayerIndex].textItem.contents = rightBook.name
+  // 改对应的社团名 因为摊位名称不是成组复制的，需要通过取余数来判断偏移(x2因为空了一行), 顺便减去left所占用的格子（20 * 2）
+  var rightBookGroupIdIndex = (root.artLayers.length - ROW_NUMBER * COL_EACH_ROW * 2) - 1 - (rightIndex % COL_EACH_ROW) - Math.floor(rightIndex / COL_EACH_ROW) * COL_EACH_ROW * 2
+  var rightBookStallNameIndex = groupIdIndex - COL_EACH_ROW
+  root.artLayers[rightBookGroupIdIndex].textItem.contents = rightBook.groupId
+  root.artLayers[rightBookStallNameIndex].textItem.contents = rightBook.stallName
+  // 同一个社团则隐藏名字
+  if (lastGroupId === rightBook.groupId) {
+    root.artLayers[rightBookGroupIdIndex].visible = false
+    root.artLayers[rightBookStallNameIndex].visible = false
+  }
+  // 哨兵赋值
+  lastGroupId = rightBook.groupId
+}
 
 /**
  * 图片填充
